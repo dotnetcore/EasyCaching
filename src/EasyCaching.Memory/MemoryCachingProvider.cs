27aa97f7122fbb1735fa35e9a94432f2b56f2083
catcherwong <catcher_hwq@outlook.com>
2017-11-14 20:50:34 +0800

    :sparkles: Add MemoryCaching Project

--- src/EasyCaching.Memory/MemoryCachingProvider.cs
+++ src/EasyCaching.Memory/MemoryCachingProvider.cs
@@ -1,10 +1,49 @@
-﻿using System;
-namespace EasyCaching.Memory
+﻿namespace EasyCaching.Memory
 {
-    public class MemoryCachingProvider
+    using System;
+    using EasyCaching.Core;
+    using Microsoft.Extensions.Caching.Memory;
+
+    /// <summary>
+    /// MemoryCaching provider.
+    /// </summary>
+    public class MemoryCachingProvider : IEasyCachingProvider
     {
-        public MemoryCachingProvider()
+        /// <summary>
+        /// The MemoryCache.
+        /// </summary>
+        private readonly IMemoryCache _cache;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:EasyCaching.Memory.MemoryCachingProvider"/> class.
+        /// </summary>
+        /// <param name="cache">Microsoft MemoryCache.</param>
+        public MemoryCachingProvider(IMemoryCache cache)
         {
+            this._cache = cache;
+        }
+
+        /// <summary>
+        /// Get cacheValue by specified cacheKey.
+        /// </summary>
+        /// <returns>The cacheValue.</returns>
+        /// <param name="cacheKey">Cache key.</param>
+        public object Get(string cacheKey)
+        {
+            if (string.IsNullOrWhiteSpace(cacheKey))
+                throw new ArgumentNullException(nameof(cacheKey));
+
+            return _cache.Get(cacheKey);
+        }
+
+        /// <summary>
+        /// Set the specified cacheEntry.
+        /// </summary>
+        /// <returns>The set.</returns>
+        /// <param name="cacheEntry">Cache entry.</param>
+        public void Set(CacheEntry cacheEntry)
+        {
+            _cache.Set(cacheEntry.CacheKey, cacheEntry.CacheValue, cacheEntry.AbsoluteExpirationRelativeToNow);
         }
     }
 }
