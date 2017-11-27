d00ede3604e75ac53c6cbbc59dcc9237b33f44fc
catcherwong <catcher_hwq@outlook.com>
2017-11-27 21:13:08 +0800

    :sparkles: Add Caching Interceptor.

---
+++ src/EasyCaching.Core/Internal/ICachable.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyCaching.Core.Internal
+{
+    /// <summary>
+    /// Cachable.
+    /// </summary>
+    public interface ICachable
+    {
+        /// <summary>
+        /// Gets the cache key.
+        /// </summary>
+        /// <value>The cache key.</value>
+        string CacheKey { get; }
+    }
+}
