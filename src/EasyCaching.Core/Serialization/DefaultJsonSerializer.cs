81c088946201cbee23a9dc48db253937d7a3d40d
catcherwong <catcher_hwq@outlook.com>
2017-12-19 22:06:17 +0800

    :sparkles:	Add Default Json Serializer.

---
+++ src/EasyCaching.Core/Serialization/DefaultJsonSerializer.cs
@@ -0,0 +1,39 @@
+﻿namespace EasyCaching.Core.Serialization
+{
+    /// <summary>
+    /// Default json serializer.
+    /// </summary>
+    public class DefaultJsonSerializer : ISerializer
+    {
+        /// <summary>
+        /// Deserialize the specified string.
+        /// </summary>
+        /// <returns>The deserialize.</returns>
+        /// <param name="str">String.</param>
+        public object Deserialize(string str)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject(str);
+        }
+
+        /// <summary>
+        /// Deserialize the specified string.
+        /// </summary>
+        /// <returns>The deserialize.</returns>
+        /// <param name="str">String.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public T Deserialize<T>(string str)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(str);
+        }
+
+        /// <summary>
+        /// Serialize the specified object.
+        /// </summary>
+        /// <returns>The serialize.</returns>
+        /// <param name="obj">Object.</param>
+        public string Serialize(object obj)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+        }
+    }
+}
