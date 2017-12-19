e269e3b9b92571b314a2ceecb97667042c225e0f
catcherwong <catcher_hwq@outlook.com>
2017-12-19 22:03:18 +0800

    :sparkles: Add ISerializer interface.

---
+++ src/EasyCaching.Core/ISerializer.cs
@@ -0,0 +1,30 @@
+﻿namespace EasyCaching.Core
+{
+    /// <summary>
+    /// Serializer.
+    /// </summary>
+    public interface ISerializer
+    {
+        /// <summary>
+        /// Serialize the specified item.
+        /// </summary>
+        /// <returns>The serialize.</returns>
+        /// <param name="obj">Object.</param>
+        string Serialize(object obj);
+
+        /// <summary>
+        /// Deserialize the specified str.
+        /// </summary>
+        /// <returns>The deserialize.</returns>
+        /// <param name="str">String.</param>
+        object Deserialize(string str);
+
+        /// <summary>
+        /// Deserialize the specified str.
+        /// </summary>
+        /// <returns>The deserialize.</returns>
+        /// <param name="str">String.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        T Deserialize<T>(string str);
+    }
+}
