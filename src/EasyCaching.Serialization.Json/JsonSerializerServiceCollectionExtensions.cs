b76e2dab8bded0d125146ef1763fae4426ce7b67
catcherwong <catcher_hwq@outlook.com>
2018-02-17 15:06:32 +0800

    :sparkles: Add Json Serializer.

---
+++ src/EasyCaching.Serialization.Json/JsonSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace EasyCaching.Serialization.Json
+{
+    using EasyCaching.Core;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+    using System;
+
+    /// <summary>
+    /// Json serializer service collection extensions.
+    /// </summary>
+    public static class JsonSerializerServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the default json serializer.
+        /// </summary>
+        /// <returns>The default json serializer.</returns>
+        /// <param name="services">Services.</param>
+        public static IServiceCollection AddDefaultJsonSerializer(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.TryAddSingleton<IEasyCachingSerializer, DefaultJsonSerializer>();
+
+            return services;
+        }
+    }
+}
---
+++ test/EasyCaching.UnitTests/SerializerTests/JsonSerializerTest.cs
@@ -0,0 +1,12 @@
+﻿namespace EasyCaching.UnitTests
+{
+    using EasyCaching.Serialization.Json;
+
+    public class JsonSerializerTest: BaseSerializerTest
+    {
+        public JsonSerializerTest()
+        {
+            _serializer = new DefaultJsonSerializer();
+        }
+    }
+}
