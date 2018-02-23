cfda6fce376a2f9e5ba7c90a47fd568c24bf802f
catcherwong <catcher_hwq@outlook.com>
2018-02-23 19:58:13 +0800

    :sparkles: Add SerializerBuilder for Protobuf Serialization.

--- src/EasyCaching.Serialization.Protobuf/ProtobufSerializerServiceCollectionExtensions.cs
+++ src/EasyCaching.Serialization.Protobuf/ProtobufSerializerServiceCollectionExtensions.cs
@@ -4,6 +4,8 @@
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.DependencyInjection.Extensions;
     using System;
+    using System.Collections.Generic;
+    using System.Reflection;
 
     /// <summary>
     /// Protobuf serializer service collection extensions.
@@ -23,6 +25,33 @@
             }
 
             services.TryAddSingleton<IEasyCachingSerializer, DefaultProtobufSerializer>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the default protobuf serializer.
+        /// </summary>
+        /// <returns>The default protobuf serializer.</returns>
+        /// <param name="services">Services.</param>
+        /// <param name="assemblys">Assemblys.</param>
+        public static IServiceCollection AddDefaultProtobufSerializer(this IServiceCollection services, IList<Assembly> assemblys)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.TryAddSingleton<IEasyCachingSerializer, DefaultProtobufSerializer>();
+
+            //Handle Protobuf Attribute
+            foreach (var assembly in assemblys)
+            {
+                foreach (Type type in assembly.GetTypes())
+                {
+                    SerializerBuilder.Build(type);
+                }
+            }
 
             return services;
         }
