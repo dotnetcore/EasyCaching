f1316c1a51eb54d03d98a95cb04ded2b912cb165
catcherwong <catcher_hwq@outlook.com>
2018-02-28 21:46:33 +0800

    :white_check_mark: Add test for Issues16.

--- test/EasyCaching.UnitTests/CachingTests/RedisCachingProviderTest.cs
+++ test/EasyCaching.UnitTests/CachingTests/RedisCachingProviderTest.cs
@@ -5,6 +5,7 @@ namespace EasyCaching.UnitTests
     using EasyCaching.Redis;
     using Microsoft.Extensions.DependencyInjection;
     using System;
+    using Xunit;
 
     public class RedisCachingProviderTest : BaseCachingProviderTest
     {
@@ -19,5 +20,27 @@ namespace EasyCaching.UnitTests
             _provider = serviceProvider.GetService<IEasyCachingProvider>();
             _defaultTs = TimeSpan.FromSeconds(30);
         }
+
+        [Fact]
+        public void Issues16_DateTimeTest()
+        {
+            var model = new Model
+            {
+                Dt = Convert.ToDateTime("2018-02-12 12:11:00")
+            };
+
+            _provider.Set("activity",model,_defaultTs);
+
+            var res = _provider.Get<Model>("activity");
+
+            Assert.Equal("2018-02-12 12:11:00", res.Value.Dt?.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+
+        [Serializable]
+        public class Model
+        {
+            public DateTime? Dt { get; set; }
+        }
+
     }
 }
