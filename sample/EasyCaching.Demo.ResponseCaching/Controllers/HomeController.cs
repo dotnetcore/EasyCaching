d727ef7b002c2b6956e48d1e1adc540a4ff510b4
catcherwong <catcher_hwq@outlook.com>
2018-03-30 20:20:33 +0800

    :sparkles: Add ResponseCachingDemo.

---
+++ sample/EasyCaching.Demo.ResponseCaching/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿namespace EasyCaching.Demo.ResponseCaching.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    
+    public class HomeController : Controller
+    {
+        [ResponseCache(Duration = 20)]
+        public IActionResult Index()
+        {
+            return View(new Models.TestModel
+            {
+                LastUpdated = DateTimeOffset.UtcNow.ToString()
+            });
+        }
+
+        public IActionResult About()
+        {
+            return View(new Models.TestModel
+            {
+                LastUpdated = DateTimeOffset.UtcNow.ToString()
+            });
+        }
+    }
+}
