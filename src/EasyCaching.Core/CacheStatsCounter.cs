b0677d32bd48d20471e0c17227a2d6f80fb80ae8
catcherwong <catcher_hwq@outlook.com>
2018-05-07 22:15:19 +0800

    :sparkles: Add Cache Stats Counter.

---
+++ src/EasyCaching.Core/CacheStatsCounter.cs
@@ -0,0 +1,48 @@
+﻿namespace EasyCaching.Core
+{
+    using System.Threading;
+
+    /// <summary>
+    /// Cache stats counter.
+    /// </summary>
+    public class CacheStatsCounter
+    {
+        /// <summary>
+        /// The hited count.
+        /// </summary>
+        private long _hitedCount = 0;
+
+        /// <summary>
+        /// The missed count.
+        /// </summary>
+        private long _missedCount = 0;
+
+        /// <summary>
+        /// Increments the hited.
+        /// </summary>
+        public void IncrementHited()
+        {
+            Interlocked.Increment(ref _hitedCount);
+        }
+
+        /// <summary>
+        /// Increments the missed.
+        /// </summary>
+        public void IncrementMissed()
+        {
+            Interlocked.Increment(ref _missedCount);
+        }
+
+        /// <summary>
+        /// Gets the hited count.
+        /// </summary>
+        /// <returns>The hited count.</returns>
+        public long GetHitedCount() => _hitedCount;
+
+        /// <summary>
+        /// Gets the missed count.
+        /// </summary>
+        /// <returns>The missed count.</returns>
+        public long GetMissedCount() => _missedCount;
+    }
+}
