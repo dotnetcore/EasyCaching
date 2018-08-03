0a0d28d9187a58085b21339932823af696d8459c
catcherwong <catcher_hwq@outlook.com>
2018-08-03 23:34:04 +0800

    :sparkles: Configurable JSON serializer

---
+++ src/EasyCaching.Serialization.Json/EasyCachingJsonSerializerOptions.cs
@@ -0,0 +1,58 @@
+﻿namespace EasyCaching.Serialization.Json
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// EasyCaching json serializer options.
+    /// </summary>
+    public class EasyCachingJsonSerializerOptions
+    {
+        /// <summary>
+        /// Gets or sets the reference loop handling.
+        /// </summary>
+        /// <value>The reference loop handling.</value>
+        public ReferenceLoopHandling ReferenceLoopHandling { get; set; } = ReferenceLoopHandling.Error;
+
+        /// <summary>
+        /// Gets or sets the type name handling.
+        /// </summary>
+        /// <value>The type name handling.</value>
+        public TypeNameHandling TypeNameHandling { get; set; } = TypeNameHandling.All;
+
+        /// <summary>
+        /// Gets or sets the metadata property handling.
+        /// </summary>
+        /// <value>The metadata property handling.</value>
+        public MetadataPropertyHandling MetadataPropertyHandling { get; set; } = MetadataPropertyHandling.Default;
+
+        /// <summary>
+        /// The missing member handling.
+        /// </summary>
+        public MissingMemberHandling MissingMemberHandling = MissingMemberHandling.Ignore;
+
+        /// <summary>
+        /// The null value handling.
+        /// </summary>
+        public NullValueHandling NullValueHandling = NullValueHandling.Include;
+
+        /// <summary>
+        /// The value handling.
+        /// </summary>
+        public DefaultValueHandling DefaultValueHandling = DefaultValueHandling.Include;
+
+        /// <summary>
+        /// The object creation handling.
+        /// </summary>
+        public ObjectCreationHandling ObjectCreationHandling = ObjectCreationHandling.Auto;
+
+        /// <summary>
+        /// The preserve references handling.
+        /// </summary>
+        public PreserveReferencesHandling PreserveReferencesHandling = PreserveReferencesHandling.None;
+
+        /// <summary>
+        /// The constructor handling.
+        /// </summary>
+        public ConstructorHandling ConstructorHandling = ConstructorHandling.Default;
+    }
+}
--- test/EasyCaching.UnitTests/SerializerTests/JsonSerializerTest.cs
+++ test/EasyCaching.UnitTests/SerializerTests/JsonSerializerTest.cs
@@ -1,12 +1,62 @@
 ﻿namespace EasyCaching.UnitTests
 {
     using EasyCaching.Serialization.Json;
+    using FakeItEasy;
+    using Microsoft.Extensions.Options;
+    using Newtonsoft.Json;
+    using Xunit;
 
-    public class JsonSerializerTest: BaseSerializerTest
+    public class JsonSerializerTest : BaseSerializerTest
     {
         public JsonSerializerTest()
         {
-            _serializer = new DefaultJsonSerializer();
+            var options = A.Fake<IOptions<EasyCachingJsonSerializerOptions>>();
+
+            A.CallTo(() => options.Value).Returns(new EasyCachingJsonSerializerOptions());
+
+            _serializer = new DefaultJsonSerializer(options);
+        }
+
+        [Fact]
+        public void Isuue_50_Test()
+        {
+            Employee joe = new Employee { Name = "Joe User" };
+            Employee mike = new Employee { Name = "Mike Manager" };
+            joe.Manager = mike;
+            mike.Manager = mike;
+
+            Assert.Throws<JsonSerializationException>(() => _serializer.Serialize(joe));
+        }
+
+        [Fact]
+        public void Options_Test_Should_Succeed()
+        {
+            var options = A.Fake<IOptions<EasyCachingJsonSerializerOptions>>();
+
+            A.CallTo(() => options.Value).Returns(new EasyCachingJsonSerializerOptions()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            var  serializer = new DefaultJsonSerializer(options);
+
+            Employee joe = new Employee { Name = "Joe User" };
+            Employee mike = new Employee { Name = "Mike Manager" };
+            joe.Manager = mike;
+            mike.Manager = mike;
+
+            var joe_byte = serializer.Serialize(joe);
+            var joe_obj = serializer.Deserialize<Employee>(joe_byte);
+
+
+            Assert.Equal(joe.Name, joe_obj.Name);
+
+        }
+
+        public class Employee
+        {
+            public string Name { get; set; }
+            public Employee Manager { get; set; }
         }
     }
 }
