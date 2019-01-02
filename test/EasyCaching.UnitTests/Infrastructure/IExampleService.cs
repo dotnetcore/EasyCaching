6e0f956298403a08c47ef3919d9ba2a6d5c7532f
catcherwong <catcher_hwq@outlook.com>
2019-01-02 22:09:10 +0800

    :construction: Update Castle Test.

--- test/EasyCaching.UnitTests/Infrastructure/IExampleService.cs
+++ test/EasyCaching.UnitTests/Infrastructure/IExampleService.cs
@@ -6,41 +6,43 @@ namespace EasyCaching.UnitTests.Infrastructure
 
     public interface ICastleExampleService
     {
+        [EasyCachingAble(Expiration = 1)]
         string GetCurrentUTC();
 
         long GetCurrentUTCTick();
 
+        [EasyCachingPut(CacheKeyPrefix = "CastleExample")]
         string PutTest(int num, string str = "123");
 
+        [EasyCachingEvict(CacheKeyPrefix = "CastleExample")]
         string EvictTest();
 
+        [EasyCachingEvict(CacheKeyPrefix = "CastleExample", IsAll = true)]
         string EvictAllTest();
 
+        [EasyCachingAble(Expiration = 1)]
         Task<string> AbleTestAsync();
 
+        [EasyCachingEvict(CacheKeyPrefix = "CastleExample")]
         Task EvictTestAsync();
 
+        [EasyCachingPut(CacheKeyPrefix = "CastleExample")]
         Task<string> PutTestAsync(int num, string str = "123");
 
     }
 
-    public class CastleExampleService : ICastleExampleService, IEasyCaching
-    {
-
-        [EasyCachingEvict(CacheKeyPrefix = "CastleExample", IsAll = true)]
+    public class CastleExampleService : ICastleExampleService//, IEasyCaching
+    {   
         public string EvictAllTest()
         {
             return "EvictAllTest";
         }
 
-        [EasyCachingEvict(CacheKeyPrefix = "CastleExample")]
         public string EvictTest()
         {
             return "EvictTest";
         }
 
-
-        [EasyCachingAble(Expiration = 1)]
         public string GetCurrentUTC()
         {
             return DateTime.UtcNow.ToString();
@@ -51,32 +53,28 @@ namespace EasyCaching.UnitTests.Infrastructure
             return DateTime.UtcNow.Ticks;
         }
 
-        [EasyCachingPut(CacheKeyPrefix = "CastleExample")]
         public string PutTest(int num, string str)
         {
             return $"PutTest-{num}";
         }
 
-        [EasyCachingAble(Expiration = 1)]
         public async Task<string> AbleTestAsync()
         {
             return await Task.FromResult("I am task");
         }
 
-        [EasyCachingEvict(CacheKeyPrefix = "CastleExample")]
         public async Task EvictTestAsync()
         {
             await Task.CompletedTask;
         }
 
-        [EasyCachingPut(CacheKeyPrefix = "CastleExample")]
         public async Task<string> PutTestAsync(int num, string str)
         {
             return await Task.FromResult($"PutTestAsync-{num}");
         }
     }
 
-    public interface IAspectCoreExampleService : IEasyCaching
+    public interface IAspectCoreExampleService //: IEasyCaching
     {
         [EasyCachingAble(Expiration = 1)]
         string GetCurrentUTC();
