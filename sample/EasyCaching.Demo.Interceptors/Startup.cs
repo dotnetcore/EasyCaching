939bd5785be114c2b85cb1b72f7069d351007b99
catcherwong <catcher_hwq@outlook.com>
2019-01-30 20:25:17 +0800

    :construction: Update Samples.

--- sample/EasyCaching.Demo.Interceptors/Startup.cs
+++ sample/EasyCaching.Demo.Interceptors/Startup.cs
@@ -1,5 +1,6 @@
 ﻿namespace EasyCaching.Demo.Interceptors
 {
+    using EasyCaching.Core;
     using EasyCaching.Demo.Interceptors.Services;
     using EasyCaching.Interceptor.AspectCore;
     using EasyCaching.Interceptor.Castle;
@@ -22,81 +23,93 @@
 
         public IConfiguration Configuration { get; }
 
-        ////1.AspectCore
+        //1.AspectCore
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IAspectCoreService, AspectCoreService>();
+
+            services.AddEasyCaching(options=> 
+            {
+                options.UseInMemory();
+
+                //options.UseRedis(config =>
+                //{
+                //    config.DBConfig = new RedisDBOptions { Configuration = "localhost" };
+                //});
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //1.1. all default
+            return services.ConfigureAspectCoreInterceptor();
+
+            //1.2. default and customize
+            //Action<IServiceContainer> action = x =>
+            //{
+            //    x.AddType<IAspectCoreService, AspectCoreService>();
+            //};
+
+            //return services.ConfigureAspectCoreInterceptor(action);
+
+            //1.3. all customize
+            //Action<IServiceContainer> action = x =>
+            //{
+            //    x.AddType<IDateTimeService, DateTimeService>();
+            //    x.Configure(config =>
+            //    {
+            //        config.Interceptors.AddTyped<EasyCachingInterceptor>(method => typeof(Core.Internal.IEasyCaching).IsAssignableFrom(method.DeclaringType));
+            //    });
+            //};
+
+            //return services.ConfigureAspectCoreInterceptor(action, true);
+        }
+
+        ////2. Castle
         //public IServiceProvider ConfigureServices(IServiceCollection services)
         //{
-        //    services.AddScoped<IAspectCoreService, AspectCoreService>();
-
-        //    services.AddDefaultInMemoryCache();
-        //    //services.AddDefaultRedisCache(options => 
-        //    //{
-        //    //    options.DBConfig = new RedisDBOptions { Configuration = "localhost" };
-        //    //});
+        //    services.AddTransient<ICastleService, CastleService>();
 
         //    services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-        //    //1.1. all default
-        //    return services.ConfigureAspectCoreInterceptor();
+        //    services.AddEasyCaching(options =>
+        //    {
+        //        options.UseInMemory();
 
-        //    //1.2. default and customize
-        //    //Action<IServiceContainer> action = x =>
+        //        //options.UseRedis(config =>
+        //        //{
+        //        //    config.DBConfig = new RedisDBOptions { Configuration = "localhost" };
+        //        //});
+        //    });
+
+        //    //2.1. all default
+        //    return services.ConfigureCastleInterceptor();
+
+        //    //2.2. default and customize
+        //    //Action<ContainerBuilder> action = x =>
         //    //{
-        //    //    x.AddType<IAspectCoreService, AspectCoreService>();
+        //    //    x.RegisterType<CastleService>().As<ICastleService>();
         //    //};
 
-        //    //return services.ConfigureAspectCoreInterceptor(action);
+        //    //return services.ConfigureCastleInterceptor(action);
 
-        //    //1.3. all customize
-        //    //Action<IServiceContainer> action = x =>
+        //    //2.3. all customize
+        //    //Action<ContainerBuilder> action = x =>
         //    //{
-        //    //    x.AddType<IDateTimeService, DateTimeService>();
-        //    //    x.Configure(config =>
-        //    //    {
-        //    //        config.Interceptors.AddTyped<EasyCachingInterceptor>(method => typeof(Core.Internal.IEasyCaching).IsAssignableFrom(method.DeclaringType));
-        //    //    });
+        //    //    x.RegisterType<DateTimeService>().As<IDateTimeService>();
+
+        //    //    var assembly = Assembly.GetExecutingAssembly();
+        //    //    x.RegisterType<EasyCachingInterceptor>();
+
+        //    //    x.RegisterAssemblyTypes(assembly)
+        //    //        .Where(type => typeof(Core.Internal.IEasyCaching).IsAssignableFrom(type) && !type.GetTypeInfo().IsAbstract)
+        //    //        .AsImplementedInterfaces()
+        //    //        .InstancePerLifetimeScope()
+        //    //        .EnableInterfaceInterceptors()
+        //    //        .InterceptedBy(typeof(EasyCachingInterceptor));
         //    //};
 
-        //    //return services.ConfigureAspectCoreInterceptor(action, true);
+        //    //return services.ConfigureCastleInterceptor(action, true);
         //}
-
-        //2. Castle
-        public IServiceProvider ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-
-            services.AddTransient<ICastleService, CastleService>();
-
-            services.AddDefaultInMemoryCache();
-
-            //2.1. all default
-            return services.ConfigureCastleInterceptor();
-
-            //2.2. default and customize
-            //Action<ContainerBuilder> action = x =>
-            //{
-            //    x.RegisterType<CastleService>().As<ICastleService>();
-            //};
-
-            //return services.ConfigureCastleInterceptor(action);
-
-            //2.3. all customize
-            //Action<ContainerBuilder> action = x =>
-            //{
-            //    x.RegisterType<DateTimeService>().As<IDateTimeService>();
-
-            //    var assembly = Assembly.GetExecutingAssembly();
-            //    x.RegisterType<EasyCachingInterceptor>();
-
-            //    x.RegisterAssemblyTypes(assembly)
-            //        .Where(type => typeof(Core.Internal.IEasyCaching).IsAssignableFrom(type) && !type.GetTypeInfo().IsAbstract)
-            //        .AsImplementedInterfaces()
-            //        .InstancePerLifetimeScope()
-            //        .EnableInterfaceInterceptors()
-            //        .InterceptedBy(typeof(EasyCachingInterceptor));
-            //};
-
-            //return services.ConfigureCastleInterceptor(action, true);
-        }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
