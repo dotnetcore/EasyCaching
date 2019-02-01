129a1cf31aa9c42f56fa6bee2cfde69b0570b521
catcherwong <catcher_hwq@outlook.com>
2019-02-01 12:03:33 +0800

    :sparkles: NullEasyCachingBus.

---
+++ src/EasyCaching.Core/Bus/NullEasyCachingBus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyCaching.Core
+{
+    public class NullEasyCachingBus : IEasyCachingBus
+    {
+        public static readonly NullEasyCachingBus Instance = new NullEasyCachingBus();
+
+        public void Dispose() { }
+               
+        public void Publish(string topic, EasyCachingMessage message)
+        {
+
+        }
+
+        public Task PublishAsync(string topic, EasyCachingMessage message, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Subscribe(string topic, Action<EasyCachingMessage> action)
+        {
+
+        }
+    }
+}
