e20fc0e3e652f1f1a42806cc7013056cf2619f09
yaorong.liang <yaorong.liang@chukou1.com>
2019-02-22 11:56:36 +0800

    :bug: Fixed some bugs of Interceptor.Castle.

---
+++ src/EasyCaching.Interceptor.Castle/TypeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace EasyCaching.Interceptor.Castle
+{
+    public static class TypeExtensions
+    {
+        private static readonly ConcurrentDictionary<TypeInfo, bool> isTaskOfTCache = new ConcurrentDictionary<TypeInfo, bool>();
+
+        public static bool IsTaskWithResult(this TypeInfo typeInfo)
+        {
+            if (typeInfo == null)
+            {
+                throw new ArgumentNullException(nameof(typeInfo));
+            }
+            return isTaskOfTCache.GetOrAdd(typeInfo, Info => Info.IsGenericType && typeof(Task).GetTypeInfo().IsAssignableFrom(Info));
+        }
+
+        public static bool IsTask(this TypeInfo typeInfo)
+        {
+            if (typeInfo == null)
+            {
+                throw new ArgumentNullException(nameof(typeInfo));
+            }
+            return typeInfo.AsType() == typeof(Task);
+        }
+        
+    }
+}
