f5785331288f908616d1e8bdcd57263939d50eeb
Alexey Zagoskin <alexey.zagoskin@campaustralia.com.au>
2019-02-28 08:26:06 +0300

    Interceptor should generate valid cache key for all basic type parameters

--- src/EasyCaching.Core/Interceptor/DefaultEasyCachingKeyGenerator.cs
+++ src/EasyCaching.Core/Interceptor/DefaultEasyCachingKeyGenerator.cs
@@ -1,6 +1,5 @@
 ﻿namespace EasyCaching.Core.Interceptor
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
@@ -11,132 +10,34 @@
     /// </summary>
     public class DefaultEasyCachingKeyGenerator : IEasyCachingKeyGenerator
     {
-        /// <summary>
-        /// The link char of cache key.
-        /// </summary>
-        private char _linkChar = ':';
+        private const char LinkChar = ':';
 
-        /// <summary>
-        /// Gets the cache key.
-        /// </summary>
-        /// <returns>The cache key.</returns>
-        /// <param name="methodInfo">Method info.</param>
-        /// <param name="args">Arguments.</param>
-        /// <param name="prefix">Prefix.</param>
         public string GetCacheKey(MethodInfo methodInfo, object[] args, string prefix)
         {
-            if(string.IsNullOrWhiteSpace(prefix))
-            {
-                var typeName = methodInfo.DeclaringType.Name;
-                var methodName = methodInfo.Name;
-
-                var methodArguments = this.FormatArgumentsToPartOfCacheKey(args);
-
-                return this.GenerateCacheKey(typeName, methodName, methodArguments);
-            }
-            else
-            {                
-                var methodArguments = this.FormatArgumentsToPartOfCacheKey(args);
-
-                return this.GenerateCacheKey(string.Empty, prefix, methodArguments);
-            }
+            var methodArguments = args?.Any() == true
+                                      ? args.Select(ParameterCacheKeys.GenerateCacheKey)
+                                      : new[] { "0" };
+            return GenerateCacheKey(methodInfo, prefix, methodArguments);
         }
 
-        /// <summary>
-        /// Gets the cache key prefix.
-        /// </summary>
-        /// <returns>The cache key prefix.</returns>
-        /// <param name="methodInfo">Method info.</param>
-        /// <param name="prefix">Prefix.</param>
         public string GetCacheKeyPrefix(MethodInfo methodInfo, string prefix)
         {
-            if (string.IsNullOrWhiteSpace(prefix))
-            {
-                var typeName = methodInfo.DeclaringType.Name;
-                var methodName = methodInfo.Name;
+            if (!string.IsNullOrWhiteSpace(prefix)) return $"{prefix}{LinkChar}";
 
-                return this.GenerateCacheKeyPrefix(typeName, methodName);
-            }
-            else
-            {                
-                return this.GenerateCacheKeyPrefix(string.Empty, prefix);
-            }
+            var typeName = methodInfo.DeclaringType?.Name;
+            var methodName = methodInfo.Name;
+
+            return $"{typeName}{LinkChar}{methodName}{LinkChar}";
         }
 
-        /// <summary>
-        /// Generates the cache key prefix.
-        /// </summary>
-        /// <returns>The cache key prefix.</returns>
-        /// <param name="first">First.</param>
-        /// <param name="second">Second.</param>
-        private string GenerateCacheKeyPrefix(string first, string second)
-        {            
-            return string.Concat(first,_linkChar,second,_linkChar).TrimStart(_linkChar);                       
-        }
-
-        /// <summary>
-        /// Formats the arguments to part of cache key.
-        /// </summary>
-        /// <returns>The arguments to part of cache key.</returns>
-        /// <param name="methodArguments">Method arguments.</param>
-        private IList<string> FormatArgumentsToPartOfCacheKey(object[] methodArguments)
+        private string GenerateCacheKey(MethodInfo methodInfo, string prefix, IEnumerable<string> parameters)
         {
-            if(methodArguments!=null && methodArguments.Length > 0)
-            {
-                return methodArguments.Select(this.GetArgumentValue).ToList();    
-            }
-            else
-            {
-                return new List<string> { "0" };
-            }
-        }
+            var cacheKeyPrefix = GetCacheKeyPrefix(methodInfo, prefix);
 
-        /// <summary>
-        /// Generates the cache key.
-        /// </summary>
-        /// <returns>The cache key.</returns>
-        /// <param name="typeName">Type name.</param>
-        /// <param name="methodName">Method name.</param>
-        /// <param name="parameters">Parameters.</param>
-        private string GenerateCacheKey(string typeName, string methodName, IList<string> parameters)
-        {
             var builder = new StringBuilder();
-
-            builder.Append(this.GenerateCacheKeyPrefix(typeName,methodName));
-
-            foreach (var param in parameters)
-            {
-                builder.Append(param);
-                builder.Append(_linkChar);
-            }
-
-            var str = builder.ToString().TrimEnd(_linkChar);
-
-            return str;
-            //using (SHA1 sha1 = SHA1.Create())
-            //{
-            //    byte[] data = sha1.ComputeHash(Encoding.UTF8.GetBytes(str));
-            //    return Convert.ToBase64String(data, Base64FormattingOptions.None);
-            //}
-        }
-
-        /// <summary>
-        /// Gets the argument value.
-        /// </summary>
-        /// <returns>The argument value.</returns>
-        /// <param name="arg">Argument.</param>
-        private string GetArgumentValue(object arg)
-        {
-            if (arg is int || arg is long || arg is string)
-                return arg.ToString();
-
-            if (arg is DateTime)
-                return ((DateTime)arg).ToString("yyyyMMddHHmmss");
-
-            if (arg is ICachable)
-                return ((ICachable)arg).CacheKey;
-
-            return null;
+            builder.Append(cacheKeyPrefix);
+            builder.Append(string.Join(LinkChar.ToString(), parameters));
+            return builder.ToString();
         }
     }
 }
