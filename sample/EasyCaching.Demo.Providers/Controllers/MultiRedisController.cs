966d4d692660126945e34efbe8bbc75f14cfd467
catcherwong <catcher_hwq@outlook.com>
2019-04-23 22:19:21 +0800

    :wheelchair: Add multi redis sample

---
+++ sample/EasyCaching.Demo.Providers/Controllers/MultiRedisController.cs
@@ -0,0 +1,30 @@
+﻿namespace EasyCaching.Demo.Providers.Controllers
+{
+    using EasyCaching.Core;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/mredis")]
+    public class MultiRedisController : Controller
+    {
+        private readonly IRedisCachingProvider _redis1;
+        private readonly IRedisCachingProvider _redis2;
+
+        public MultiRedisController(IEasyCachingProviderFactory factory)
+        {
+            this._redis1 = factory.GetRedisProvider("redis1");
+            this._redis2 = factory.GetRedisProvider("redis2");
+        }
+
+        // GET api/mredis
+        [HttpGet]
+        public string Get()
+        {
+            _redis1.StringSet("keyredis1", "val");
+
+            var res1 = _redis1.StringGet("keyredis1");
+            var res2 = _redis2.StringGet("keyredis1");
+
+            return $"redis1 cached value: {res1}, redis2 cached value : {res2}";               
+        }             
+    }
+}
