9d28611d714cb4670c84ea751b07c2290f3d7473
ydy <517697206@qq.com>
2023-03-07 15:34:56 +0800

    feat: 1.测试杨丽etcd

--- sample/EasyCaching.Demo.ConsoleApp/Program.cs
+++ sample/EasyCaching.Demo.ConsoleApp/Program.cs
@@ -6,10 +6,13 @@ namespace EasyCaching.Demo.ConsoleApp
     using EasyCaching.Disk;
     using EasyCaching.Serialization.SystemTextJson.Configurations;
     using EasyCaching.SQLite;
+    using Google.Protobuf.WellKnownTypes;
     using MemoryPack;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Options;
+    using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
     using System.IO;
 
     class Program
@@ -21,62 +24,64 @@ namespace EasyCaching.Demo.ConsoleApp
             IServiceCollection services = new ServiceCollection();
             services.AddEasyCaching(option =>
             {
-                //option.WithMemoryPack(configure =>
-                //{
-                //}, "mempack");
+                option.WithMemoryPack(configure =>
+                {
+                }, "mempack");
 
                 option.UseInMemory("m1");
 
                 option.UseEtcd(options =>
                 {
-                    options.Address = "http://192.168.0.75:2379";
+                    options.Address = "http://127.0.0.1:2379";
+                    options.ServerName = "";
                     options.Timeout = 30000;
                     options.SerializerName= "json";
-                },"e1").WithJson("json");
+                }, "e1").WithJson("json");
 
-                //option.UseRedis((options) =>
-                //{
-                //    options.SerializerName = "mempack";
-                //    options.DBConfig.Endpoints.Add(new Core.Configurations.ServerEndPoint("localhost", 6388));
-                //}, "r1");
+                option.UseRedis((options) =>
+                {
+                    options.SerializerName = "mempack";
+                    options.DBConfig.Endpoints.Add(new Core.Configurations.ServerEndPoint("localhost", 6388));
+                }, "r1");
 
-                //option.UseSQLite(c =>
-                //{
-                //    c.DBConfig = new SQLiteDBOptions
-                //    {
-                //        FileName = "demo.db",
-                //        CacheMode = Microsoft.Data.Sqlite.SqliteCacheMode.Default,
-                //        OpenMode = Microsoft.Data.Sqlite.SqliteOpenMode.Memory,
-                //    };
-                //}, "s1");
+                option.UseSQLite(c =>
+                {
+                    c.DBConfig = new SQLiteDBOptions
+                    {
+                        FileName = "demo.db",
+                        CacheMode = Microsoft.Data.Sqlite.SqliteCacheMode.Default,
+                        OpenMode = Microsoft.Data.Sqlite.SqliteOpenMode.Memory,
+                    };
+                }, "s1");
 
-                //// option.WithJson(jsonSerializerSettingsConfigure: x =>
-                //// {
-                ////     x.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None;
-                //// }, "json");
+                option.WithJson(jsonSerializerSettingsConfigure: x =>
+                {
+                    x.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None;
+                    x.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                }, "json");
 
-                //option.UseDisk(cfg =>
-                //{
-                //    cfg.DBConfig = new DiskDbOptions { BasePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Cache") };
-                //    cfg.SerializerName = "msgpack";
-                //}, "disk")
-                //.WithJson("json")
-                //.WithSystemTextJson("sysjson")
-                //.WithMessagePack("msgpack");
+                option.UseDisk(cfg =>
+                {
+                    cfg.DBConfig = new DiskDbOptions { BasePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Cache") };
+                    cfg.SerializerName = "msgpack";
+                }, "disk")
+                .WithJson("json")
+                .WithSystemTextJson("sysjson")
+                .WithMessagePack("msgpack");
             });
 
             IServiceProvider serviceProvider = services.BuildServiceProvider();
             var factory = serviceProvider.GetService<IEasyCachingProviderFactory>();
 
-            // var redisCache = factory.GetCachingProvider("r1");
-            //
-            // redisCache.Set<Product>("rkey", new Product() { Name = "test" }, TimeSpan.FromSeconds(20));
-            //
-            // var redisAllKey = redisCache.GetAllKeysByPrefix("rkey");
-            //
-            // var redisVal = redisCache.Get<Product>("rkey");
-            //
-            // Console.WriteLine($"redis cache get value, {redisVal.HasValue} {redisVal.IsNull} {redisVal.Value}");
+            var redisCache = factory.GetCachingProvider("r1");
+
+            redisCache.Set<Product>("rkey", new Product() { Name = "test" }, TimeSpan.FromSeconds(20));
+
+            var redisAllKey = redisCache.GetAllKeysByPrefix("rkey");
+
+            var redisVal = redisCache.Get<Product>("rkey");
+
+            Console.WriteLine($"redis cache get value, {redisVal.HasValue} {redisVal.IsNull} {redisVal.Value}");
 
             var prod = new Product()
             {
@@ -90,31 +95,39 @@ namespace EasyCaching.Demo.ConsoleApp
             };
             prod.Inner.Inner = prod;
 
+            // redis cache
+            var rCache = factory.GetCachingProvider("r1");
+            rCache.Set<Product>("mkey1", prod, TimeSpan.FromSeconds(20));
+            var mVal1 = rCache.Get<Product>("mkey1");
+            rCache.Set<string>("mkey", "mvalue", TimeSpan.FromSeconds(20));
+            var mVal = rCache.Get<string>("mkey");
+            var mAllKey = rCache.GetAllKeysByPrefix("mk");
+            Console.WriteLine($"redis cache get value, {mVal.HasValue} {mVal.IsNull} {mVal.Value} ");
+
+            // sqllite cache
+            var sCache = factory.GetCachingProvider("s1");
+            sCache.Set<string>("skey", "svalue", TimeSpan.FromSeconds(20));
+            var sVal = sCache.Get<string>("skey");
+            Console.WriteLine($"sqlite cache get value, {sVal.HasValue} {sVal.IsNull} {sVal.Value} ");
+
+            // disk cache
+            var diskCache = factory.GetCachingProvider("disk");
+            diskCache.Set<string>("diskkey", "diskvalue", TimeSpan.FromSeconds(20));
+            var diskVal = diskCache.Get<string>("diskkey");
+            Console.WriteLine($"disk cache get value, {diskVal.HasValue} {diskVal.IsNull} {diskVal.Value} ");
+
             //etcd cache
             var etcdCache = factory.GetCachingProvider("e1");
-            etcdCache.Set<Product>("emkey2", prod, TimeSpan.FromSeconds(2000));
-            var eVal1 = etcdCache.Get<Product>("emkey2");
-
-            //// redis cache
-            //var rCache = factory.GetCachingProvider("r1");
-            //rCache.Set<Product>("mkey1", prod, TimeSpan.FromSeconds(20));
-            //var mVal1 = rCache.Get<Product>("mkey1");
-            //rCache.Set<string>("mkey", "mvalue", TimeSpan.FromSeconds(20));
-            //var mVal = rCache.Get<string>("mkey");
-            //var mAllKey = rCache.GetAllKeysByPrefix("mk");
-            //Console.WriteLine($"redis cache get value, {mVal.HasValue} {mVal.IsNull} {mVal.Value} ");
-
-            //// sqllite cache
-            //var sCache = factory.GetCachingProvider("s1");
-            //sCache.Set<string>("skey", "svalue", TimeSpan.FromSeconds(20));
-            //var sVal = sCache.Get<string>("skey");
-            //Console.WriteLine($"sqlite cache get value, {sVal.HasValue} {sVal.IsNull} {sVal.Value} ");
-
-            //// disk cache
-            //var diskCache = factory.GetCachingProvider("disk");
-            //diskCache.Set<string>("diskkey", "diskvalue", TimeSpan.FromSeconds(20));
-            //var diskVal = diskCache.Get<string>("diskkey");
-            //Console.WriteLine($"disk cache get value, {diskVal.HasValue} {diskVal.IsNull} {diskVal.Value} ");
+            var re11 = etcdCache.GetAllKeysByPrefix("emk");
+            var re12 = etcdCache.GetByPrefix<Product>("emk");
+            etcdCache.Set<Product>("emkey3", prod, TimeSpan.FromSeconds(2000));
+            var re13 = etcdCache.Get<Product>("emkey3");
+            var re14 = etcdCache.GetAll<Product>(new List<string>()
+            {
+                "emkey3"
+            });
+            etcdCache.Remove("emkey3");
+            Console.WriteLine($"etcd cache get value, {re13.HasValue} {re13.IsNull} {re13.Value} ");
 
             Console.ReadKey();
         }
