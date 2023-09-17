02c5166f0fe680b1e1b40b52d5900f519368d3aa
erfanmahmoudi98@outlook.com <erfanmahmoudi98@outlook.com>
2023-09-17 11:30:16 +0330

    Add locks sample

---
+++ sample/EasyCaching.Demo.Locks/Controllers/LocksController.cs
@@ -0,0 +1,70 @@
+﻿namespace EasyCaching.Demo.Locks.Controllers;
+
+[Route("api/[controller]")]
+public class LocksController : Controller
+{
+    private readonly IDistributedLockFactory _distributedLockFactory;
+    
+    public LocksController(IDistributedLockFactory distributedLockFactory)
+    {
+        _distributedLockFactory = distributedLockFactory;
+    }
+
+    [HttpPost("distributed-locking")]
+    public async Task DistributedLockingOperation(int millisecondsTimeout)
+    {
+        using var distributedLock = _distributedLockFactory.CreateLock("DefaultRedis", "YourKey");
+
+        try
+        {
+            if (await distributedLock.LockAsync(millisecondsTimeout))
+            {
+                // Simulate operation
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                // Proper error
+            }
+        }
+        catch (Exception ex)
+        {
+            // log error
+            throw;
+        }
+        finally
+        {
+            // release lock at the end
+            await distributedLock.ReleaseAsync();
+        }
+    }
+
+    [HttpPost("memory-locking")]
+    public async Task MemoryLockingOperation(int millisecondsTimeout)
+    {
+        using var memoryLock = _distributedLockFactory.CreateLock("DefaultInMemory", "YourKey");
+
+        try
+        {
+            if (await memoryLock.LockAsync(millisecondsTimeout))
+            {
+                // Simulate operation
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                // Proper error
+            }
+        }
+        catch (Exception ex)
+        {
+            // log error
+            throw;
+        }
+        finally
+        {
+            // release lock at the end
+            await memoryLock.ReleaseAsync();
+        }
+    }
+}
---
+++ sample/EasyCaching.Demo.Locks/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using EasyCaching.Core.Configurations;
+global using EasyCaching.Core.DistributedLock;
+global using Microsoft.AspNetCore.Mvc;
+global using EasyCaching.Redis.DistributedLock;
