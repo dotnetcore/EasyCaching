81d13f7b4e116c80e7e1e5566d3789332aeddfe7
Catcher Wong <catcher_hwq@outlook.com>
2023-11-13 09:22:32 +0000

    revert #487 (#504)
    Signed-off-by: catcherwong <catcher_hwq@outlook.com>

--- src/EasyCaching.Core/Configurations/EasyCachingServiceCollectionExtensions.cs
+++ src/EasyCaching.Core/Configurations/EasyCachingServiceCollectionExtensions.cs
@@ -30,38 +30,5 @@
 
             return services;
         }
-        
-        /// <summary>
-        /// Adds the EasyCaching.
-        /// </summary>
-        /// <returns>The easy caching.</returns>
-        /// <param name="services">Services.</param>
-        /// <param name="setupAction">Setup action.</param>
-        public static IServiceCollection AddEasyCaching(this IServiceCollection services, Action<IServiceProvider, EasyCachingOptions> setupAction)
-        {
-            ArgumentCheck.NotNull(setupAction, nameof(setupAction));
-
-            // Options
-            services.AddSingleton(sp =>
-            {
-                var options = new EasyCachingOptions();
-                setupAction(sp, options);
-                return options;
-            });
-
-            // Extension services
-            services.AddSingleton(sp =>
-            {
-                var options = sp.GetRequiredService<EasyCachingOptions>();
-                foreach (var serviceExtension in options.Extensions)
-                {
-                    serviceExtension.AddServices(services);
-                }
-                
-                return options;
-            });
-
-            return services;
-        }
     }
 }
